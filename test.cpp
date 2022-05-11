96608415b657170c290c4b367e9a4b9174007069
Alvise Favero <alvifa02@protonmail.com>
2022-05-11 19:34:44 +0200

    Test

--- test.cpp
+++ test.cpp
@@ -6,6 +6,11 @@ using std::endl;
 
 int main()
 {
-    Player p;
-    p.init_board("board.txt");
+    List<int> l;
+    l.push_front(1);
+    l.push_front(0);
+    l.push_back(2);
+    l.push_back(3);
+    for (int i : l)
+        cout << i << endl;
 }
