b776fc72f2f6424718838d9b8ec9f1a2fab6ed75
Alvise Favero <alvifa02@protonmail.com>
2022-05-25 20:52:33 +0200

    test.cpp

--- test.cpp
+++ test.cpp
@@ -6,5 +6,15 @@ using std::endl;
 
 int main()
 {
-    
+    ifstream file{ "board_1.txt" };
+    Board b{ file };
+    Position p{ 2, 3 };
+    List<Move> moves = b.getPossibleMoves(p);
+    for (Move m : moves)
+    {
+        m.from.print(cout);
+        cout << ' ';
+        m.to.print(cout);
+        cout << endl;
+    }
 }
